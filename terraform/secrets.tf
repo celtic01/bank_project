fc15a6ec66400ef00ea74fd81a6e84e541b44646
celtic01 <andrei.bortas@student.unitbv.ro>
2023-12-07 10:47:14 +0200

    small changes

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -2,17 +2,18 @@ module "secrets_manager" {
   source = "terraform-aws-modules/secrets-manager/aws"
 
   # Secret
-  name        = "simple-bank"
+  name        = "simple-bank-env"
   description = "app properties for simple-bank project"
 
   # Policy
   block_public_policy = true
 
   secret_string = jsonencode({
-    DB_SOURCE             = format("postgresql://%s:%s@%s/simple_bank?sslmode=disable", module.db.db_instance_username, jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)["password"], module.db.db_instance_endpoint)
+    DB_SOURCE             = format("\"postgresql://%s:%s@%s/simple_bank?sslmode=disable\"", module.db.db_instance_username, jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)["password"], module.db.db_instance_endpoint)
     DB_DRIVER             = "postgres",
     SERVER_ADDRESS        = "0.0.0.0/8080",
     ACCESS_TOKEN_DURATION = 15,
     TOKEN_SYMMETRIC_KEY   = random_string.symmetric_key.id
   })
+  recovery_window_in_days = 0
 }
